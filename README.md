2998a5760f9630095dc4039a484f1596874a99da
Sanghoon Kang <53715807+totohoon02@users.noreply.github.com>
2023-12-31 20:44:04 +0900

    2023-12-31-강상훈: 날짜 시간 전까지
    # 날짜 시간 전까지

--- README.md
+++ README.md
@@ -3,3 +3,26 @@
 - `src`: sourceFile 폴더
 - `lib`: dependency 폴더
 - `bin`: 빌드하면 바이너리 파일이 저장될 곳, 커스텀하고 싶으면 `setting.json` 수정하면 된다고 함
+
+#### 자바 문법
+
+- 출력 스니펫 `pln`
+- 상수 키워드: `final`
+- 참조 자료형: 기본 자료형이 아닌 모든 자료형, 클래스의 인스턴스, 배열도 참조 자료형이다.
+- char의 연산은 아스키코드 값으로 들어간다.
+- 인덱스 없는 반복문
+
+  ```
+    int[] numbers = new int[] { 1, 2, 3, 4, 5, 7, 8, 9, 10 };
+          for (int number : numbers)
+              System.err.print(number + " ");
+  ```
+
+- Java는 메소드도 소문자로 시작
+- 상속 키워드: `extends`
+- 프로퍼티에 키워드를 안쓰면 public
+- 메소드 오버라이드 키워드: `@Override` 명확하게 나타내기 위해서 써야한다!
+- 네임스페이스 키워드 `package`, 패키지 폴더명도 같아야 함
+- .java 파일의 파일명과 내부 클래스의 이름은 동일해야 한다, 대소문자 포함
+- 임포트 키워드 `import`
+- `interface` 키워드 `implements`
--- src/App.java
+++ src/App.java
@@ -1,5 +1,74 @@
+import pkg.PackageTest;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // 자료형
+        final int NUMBER = 777;
+        boolean isTrue = true;
+        String stc = "This is String";
+
+        // 참조 자료형
+        int[] intArray = new int[] { 1, 2, 3, 4, 5 };
+
+        // Char, Stiring
+        String myString = "H";
+        char myChar = 'H';
+        System.err.println(myString + 10); // H10
+        System.err.println(myChar + 10); // 82
+
+        // Condit, Loop
+        if (isTrue)
+            System.out.println("isTrue");
+        for (int i = 0; i < 10; i++)
+            System.out.print(i + " ");
+        System.out.println("");
+        int[] numbers = new int[] { 1, 2, 3, 4, 5, 7, 8, 9, 10 };
+        for (int number : numbers)
+            System.err.print(number + " ");
+
+        // 클래스
+        IPhone iphone = new IPhone("IPhone", "Red", 1001010);
+        System.out.println(iphone.getModel());
+
+        PackageTest test = new PackageTest();
+
+        // 날짜 시간
+    }
+}
+
+class IPhone extends Phone {
+
+    IPhone(String model, String color, int price) {
+        super(model, color, price);
+    }
+
+    @Override
+    public String getModel() {
+        System.out.println(this.model);
+        return this.model;
+    }
+}
+
+class Phone {
+    String model;
+    String color;
+    int price;
+
+    Phone(String model, String color, int price) {
+        this.model = model;
+        this.color = color;
+        this.price = price;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getPrice() {
+        return price;
     }
 }
